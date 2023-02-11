8a89320e82539f34958377ea5535a670bbf81aef
Yulia Fedorova <yulia1431@yandex.ru>
2023-02-11 18:34:22 +0300

    solving task

--- task/Program.cs
+++ task/Program.cs
@@ -15,11 +15,44 @@ void PrintArray(string[] arr)
 {
     for (int i = 0; i < arr.Length; i++)
     {
-        Console.Write($"{arr[i]} ");
+        Console.WriteLine($"Элемент {i} = {arr[i]} ");
     }
 }
+
+int NumberOfElementsIf(string[] array)
+{
+    int m = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        string a = array[i];
+        if (a.Length <= 3)
+        {
+            m = m + 1;
+        }
+    }
+    return m;
+}
+
 Console.Write("Введите длину массива, который вы хотите задать: ");
 int num = int.Parse(Console.ReadLine()!);
 string[] array = GetArray(num);
 PrintArray(array);
 Console.WriteLine();
+
+//var NewArray = array.Where(s => s.Length <= 3).ToArray(); 
+//PrintArray(NewArray);
+
+int m = NumberOfElementsIf(array);
+string[] Newarray = new string[m];
+int j = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    string a = array[i];
+    if (a.Length <= 3)
+    {
+        Newarray[j] = a;
+        j = j + 1;
+    }
+}
+
+PrintArray(Newarray);
